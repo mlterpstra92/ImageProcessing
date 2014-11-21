1e21dc30087bfa7fd7e7eceb5fc67b0c0b6ff3eb
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2014-11-21 16:46:09 +0100

    more updates

--- week1/IPfilter.m
+++ week1/IPfilter.m
@@ -1,22 +1,26 @@
-function convolutedImage = IPfilter(image, mask)
-%IPFILTER Summary of this function goes here
-%   Detailed explanation goes here
+function convoluted = IPfilter(image, mask)
+    %IPFILTER Convolutes image with kernel mask
     [b, a] = (size(mask));
     b = floor(b / 2);
     a = floor(a / 2);
     [rows, cols] = size(image);
-    paddedImage = zeros(rows + 2 * b, cols + 2 * a);
-    paddedImage(1 + b:end - b, 1 + a:end - a) = image;
-    convolutedImage = zeros(size(paddedImage));
+    % Pad image for convolution. Padding is half of the kernel size
+    % at each side of the image, so in this case 2*b, 2*a
+    padded = zeros(rows + 2 * b, cols + 2 * a);
+    padded(1 + b:end - b, 1 + a:end - a) = image;
+    % store here the convoluted image
+    convoluted = zeros(size(padded));
     tic;
-    for x=1 + b:size(convolutedImage, 2) - b
-        for y=1 + a:size(convolutedImage, 1) - a
-            convolutedImage(y, x) = sum(sum(mask .* paddedImage(y - a : y + a, x - b : x + b)));
+    % Perform convolution
+    for x=1 + b:size(convoluted, 2) - b
+        for y=1 + a:size(convoluted, 1) - a
+            convoluted(y, x) = sum(sum(mask .* padded(y - a:y + a, x - b:x + b)));
         end
     end
     elapsedTime = toc;
     disp(['Total execution time: ', num2str(elapsedTime)]);
     disp(['Convolution time per pixel: ', num2str(elapsedTime / (rows * cols))]);
-    convolutedImage = convolutedImage(1+b:end - b, 1 + a:end - a);
+    % Remove padding
+    convoluted = convoluted(1+b:end - b, 1 + a:end - a);
 end
 
