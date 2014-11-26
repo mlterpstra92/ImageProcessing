65a5290e74a4098639d8afd65ffd5c9e4cba241d
Han <hankruiger@gmail.com>
2014-11-26 16:18:43 +0100

    Added difference of sharp/blur vs blur/sharp

--- week1/sharpenblur.m
+++ week1/sharpenblur.m
@@ -1,17 +1,23 @@
 sharpenMask = [0, 1, 0; 1, -4, 1; 0, 1, 0];
 blurMask = (1/5) * [0, 1, 0; 1, 1, 1; 0, 1, 0];
-image = readDoubleImage('blobs');
+image = readDoubleImage('angio_noise');
 firstBlur = IPfilter(IPfilter(image, blurMask), sharpenMask) + image;
 firstSharpen = IPfilter(IPfilter(image, sharpenMask) + image, blurMask);
+difference = abs(firstBlur - firstSharpen);
+difference = difference / max(max(difference));
 
 subplot(2, 2, 1);
 imshow(image);
 title(gca, 'original');
 
-subplot(2, 2, 3);
+subplot(2, 2, 2);
 imshow(firstBlur);
 title(gca, 'blur -> sharpen');
 
 subplot(2, 2, 4);
 imshow(firstSharpen);
 title(gca, 'sharpen -> blur');
+
+subplot(2, 2, 3);
+imshow(difference);
+title(gca, 'normalized difference');
