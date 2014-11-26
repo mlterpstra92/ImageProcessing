352d85eeff684a3ad542c2b34f44bdae27f91ea8
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2014-11-26 16:17:42 +0100

    Expanded report, and did some things

--- week1/IPgradient.m
+++ week1/IPgradient.m
@@ -1,8 +1,6 @@
-function outImage = IPgradient( image )
-%IPGRADIENT Summary of this function goes here
-%   Detailed explanation goes here
+function filter = IPgradient(image)
     sobelHor = [-1, -2, -1; 0,0,0; 1, 2, 1];
     sobelVert = sobelHor';
-    outImage = abs(IPfilter(image, sobelHor)) + abs(IPfilter(image, sobelVert));
+    filter = abs(IPfilter(image, sobelHor)) + abs(IPfilter(image, sobelVert));
 end
 
