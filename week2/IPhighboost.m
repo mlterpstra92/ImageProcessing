da2c4ef26573ab74709c824b81913382cf4ffdec
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2014-12-02 11:48:10 +0100

    Written part A

--- week2/IPhighboost.m
+++ week2/IPhighboost.m
@@ -1,11 +1,4 @@
 function outImage = IPhighboost( image, k )
-%IPHIGHBOOST Summary of this function goes here
-%   Detailed explanation goes here
-% Image can contain negative values after this function if for example
-% k >= 1 and image is: 
-%    6     6     6
-%    6     2     6
-%    6     6     6
     gaussianMask = 1/9*[1, 1, 1; 1, 1, 1; 1, 1, 1];
     blurredImage = IPfilter(image, gaussianMask);
     mask = image - blurredImage;
