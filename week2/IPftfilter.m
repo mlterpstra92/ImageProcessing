68c97e1535ba3aad0078e950f638ce5617fca67c
hankruiger <hankruiger@gmail.com>
2014-12-03 21:39:19 +0100

    Finished report IMO (except workload section)

--- week2/IPftfilter.m
+++ week2/IPftfilter.m
@@ -1,4 +1,4 @@
-function outImage = IPftfilter( image, D0, filter_type, P, Q)
+function outImage = IPftfilter( image, D_0, filter_type, P, Q)
     if nargin < 4
         P = size(image, 1);
         Q = size(image, 2);
@@ -11,12 +11,27 @@ function outImage = IPftfilter( image, D0, filter_type, P, Q)
     for u=1:P
         for v=1:Q
             if strcmp(filter_type, 'GLPF')
-                filter(u, v) = IPgaussian_lowpass(P, Q, u, v, D0);
+                filter(u, v) = IPgaussian_lowpass(P, Q, u, v, D_0);
             elseif strcmp(filter_type, 'GHPF')
-                filter(u, v) = IPgaussian_highpass(P, Q, u, v, D0);
+                filter(u, v) = IPgaussian_highpass(P, Q, u, v, D_0);
             end
         end
     end
-    outImage = abs(ifft2(ifftshift(filter .* fftRepresentation)));
+    % Multiply all pixels by the filter value.
+    outImage = filter .* fftRepresentation;
+    
+    % Inverse shift, and transform back to spatial domain.
+    outImage = ifft2(ifftshift(outImage));
+    
+    % Take only the real part, since that was what our original image was.
+    outImage = real(outImage);
+    
+    % Reslice the image (if P and Q were higher than the image size)
+    if P > size(image, 1)
+        outImage = outImage(1:size(image, 1), :);
+    end
+    if Q > size(image, 2)
+        outImage = outImage(:, 1:size(image, 2));
+    end
 end
 
--- week2/IPgaussian_highpass.m
+++ week2/IPgaussian_highpass.m
@@ -1,5 +1,5 @@
-function H = IPgaussian_highpass(P, Q, u, v, d0)
+function H = IPgaussian_highpass(P, Q, u, v, D_0)
     % The distance from the origin
     dist = sqrt((u - P / 2)^2 + (v - Q / 2)^2);
-    H = 1 - exp(-(dist^2) / (2 * d0^2));
+    H = 1 - exp(-(dist^2) / (2 * D_0^2));
 end
