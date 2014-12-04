5e6c9c6522f21d34e68006c34e0baf12ad373881
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2014-12-04 16:45:53 +0100

    Solved exercise one

--- week3/IPdwt.m
+++ week3/IPdwt.m
@@ -2,10 +2,8 @@ function res = IPdwt( fVector )
     if size(fVector, 1) == 1
         fVector = fVector';
     end
-    haar4Matrix = [1, 1 , sqrt(2) , 0       ;
-                   1, 1 , -sqrt(2), 0       ;
-                   1, -1, 0       , sqrt(2) ;
-                   1, -1, 0       , -sqrt(2)] / 2;
+               
+    haarMatrix = generateHaarMatrix(size(fVector, 1));
 
-    res = haar4Matrix\fVector;
+    res = haarMatrix'\fVector;
 end
--- week3/IPidwt.m
+++ week3/IPidwt.m
@@ -2,9 +2,6 @@ function res = IPidwt( haarFactors )
     if size(haarFactors, 1) == 1
         haarFactors = haarFactors';
     end
-    haar4Matrix = ([1      , 1       , 1      , 1; 
-                   1      , 1       , -1     , -1; 
-                   sqrt(2), -sqrt(2), 0      , 0; 
-                   0      , 0       , sqrt(2), -sqrt(2)]) / 2;
-    res = haar4Matrix\haarFactors;
+    haarMatrix = generateHaarMatrix(size(haarFactors, 1));
+    res = haarMatrix\haarFactors;
 end
