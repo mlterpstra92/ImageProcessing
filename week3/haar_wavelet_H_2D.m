5475e3049b8b5713ca74979ee68d55927093fac2
hankruiger <hankruiger@gmail.com>
2014-12-08 23:18:40 +0100

    Made 2D Haar scaling functions and wavelet functions

---
+++ week3/haar_wavelet_H_2D.m
@@ -0,0 +1,15 @@
+function phi = haar_wavelet_H_2D(x, y, j, m, n)
+%HAAR_SCALING_2D Returns the Haar scaling function on the domain x, y, with
+%   parameters j, m and n.
+    if nargin > 2
+        if nargin < 4
+            m = 0;
+            n = 0;
+        end
+        % See Eq. 7.5-6
+        phi = 2^(j / 2) * haar_wavelet_H_2D(2^j * x - m, 2^j * y - n);
+        return;
+    end
+    % This is Eq. 7.5-2.
+    phi = haar_wavelet(x)' * haar_scaling(y);
+end
