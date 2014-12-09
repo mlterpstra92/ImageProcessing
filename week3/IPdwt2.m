31384885060195c96c2c760f36e64f71bbaaa912
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2014-12-09 15:38:44 +0100

    freaks less out at multiple recursion levels

--- week3/IPdwt2.m
+++ week3/IPdwt2.m
@@ -10,8 +10,8 @@ function finalImage = IPdwt2(image, j, finalImage)
     finalImage(1:M/2, N/2+1:N) = Wh;
     finalImage(M/2+1:M, N/2+1:N) = Wd;
     if j == 1
-        finalImage(1:M/2, 1:N/2) = Wa;
+        finalImage(1:M/2, 1:N/2) = Wa / 2;
     else
-        finalImage = IPdwt2(Wa, j - 1, finalImage);
+        finalImage = IPdwt2(Wa / 2, j - 1, finalImage);
     end
 end
--- week3/IPidwt2.m
+++ week3/IPidwt2.m
@@ -1,19 +1,21 @@
 function outImage = IPidwt2(image, j)
-    outImage = image;
     if j == 0
-        return;
+        outImage = image;
+    else
+        [M, N] = size(image);
+        xWidth = M / (2^j);
+        yWidth = N / (2^j);
+        Wa = image(1:xWidth         , 1:yWidth);
+        Wh = image(xWidth+1:2*xWidth, 1:yWidth);
+        Wv = image(1:xWidth         , yWidth+1:2*yWidth);
+        Wd = image(xWidth+1:2*xWidth, yWidth+1:2*yWidth);
+        ding1 = compose_2d(Wa, Wh, 'rows');
+        ding2 = compose_2d(Wv, Wd, 'rows');
+        imshow(ding1, [0 255]); figure;
+        imshow(ding2, [0 255]); figure;
+        image(1:xWidth * 2, 1:yWidth * 2) =  compose_2d(ding1, ding2, 'columns');
+
+        outImage = IPidwt2(image * 2, j - 1);
     end
-    
-    [M, N] = size(image);
-    xWidth = M / (2^j);
-    yWidth = N / (2^j);
-    Wa = image(1:xWidth, 1:yWidth);
-    Wh = image(xWidth+1:2*xWidth, 1:yWidth);
-    Wv = image(1:xWidth, yWidth+1:2*yWidth);
-    Wd = image(xWidth+1:2*xWidth, yWidth+1:2*yWidth);
-    ding1 = compose_2d(Wa, Wh, 'rows');
-    ding2 = compose_2d(Wv, Wd, 'rows');
-    
-    outImage = IPidwt2(compose_2d(ding1, ding2, 'columns'), j - 1);
 end
 
