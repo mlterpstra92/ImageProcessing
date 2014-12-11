fd7265c86ea0c2a600054213ec0108b74e05d9e0
hankruiger <hankruiger@gmail.com>
2014-12-11 13:54:45 +0100

    Some spaces etc

--- week3/performFiltering.m
+++ week3/performFiltering.m
@@ -1,24 +1,32 @@
 function filteredImage = performFiltering(noisyWavelets, j, threshold, version)
     if j == 0
+        % We're done, since all levels that had to be filtered are
+        % filtered.
         filteredImage = noisyWavelets;
         return;
     else
         [M, N] = size(noisyWavelets);
+        
+        % Only select the details we want to filter in this level, and
+        % store the coarser details (and approximation) for higher
+        % recursions.
         res = noisyWavelets(1:M / 2, 1:N/2);
-        %Only select the details we want to filter in this level
         noisyWavelets(1:M / 2, 1:N / 2) = zeros(M/2, N/2);
         
-        %perform hard filtering by default
+        % Perform hard filtering by default
         noisyWaveletsFiltered = noisyWavelets .* (abs(noisyWavelets) > threshold);
-        if strcmp(version, 'soft') == 1
-            %correct hard filtering if soft filtering is required
+        if strcmp(version, 'soft')
+           % Correct hard filtering if soft filtering is required
            tmp = abs(noisyWaveletsFiltered) - threshold;
            tmp = (tmp+abs(tmp))/2;
            noisyWaveletsFiltered = sign(noisyWavelets) .* tmp;
         end
-        %Place filtered details back in the image
+        
+        % Place filtered details back in the image
         filteredImage = noisyWaveletsFiltered;
-        %Set the rest of the image to the recursed filtered details
+        
+        % Set the rest of the image to be the recursively computed
+        % filtering.
         filteredImage(1:M/2, 1:N/2) = performFiltering(res, j - 1, threshold, version);
     end
 end
