957c470211fac7edbddabbab7e0bceff5bc7a565
hankruiger <hankruiger@gmail.com>
2014-12-11 13:51:28 +0100

    Commented function

--- week3/IPwaveletdenoise.m
+++ week3/IPwaveletdenoise.m
@@ -1,16 +1,22 @@
 function denoisedImage = IPwaveletdenoise(image, j, threshold, version)
-    %default to hard filtering
     if nargin < 4
+        % Default to hard filtering
         version = 'hard';
     end
-    %compute the wavelets
+    
+    % Compute the wavelets
     noisyWavelets = IPdwt2(image, j);
     [M, N] = size(noisyWavelets);
-    %store the approximation for later as it is removed in the recursion
+    
+    % Store the approximation for later as it is removed in the recursion
     approx = noisyWavelets(1:M/(2^j), 1:N/(2^j));
-    %filter recursively up to level j
+    
+    % Filter recursively up to level j
     filteredImage = performFiltering(noisyWavelets, j, threshold, version);
-    %place the approximation back
+    
+    % Restore the original approximation
     filteredImage(1:M/(2^j), 1:N/(2^j)) = approx;
+    
+    % Return to the normal image representation
     denoisedImage = IPidwt2(filteredImage, j);
 end
