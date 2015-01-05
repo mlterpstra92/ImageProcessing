94e20d00123bd117ad19084db4d818b9673b5993
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2015-01-05 14:10:40 +0100

    Works

--- week4/IPclassify.m
+++ week4/IPclassify.m
@@ -1,44 +1,33 @@
 function outImage = IPclassify(threshold)
+    %Threshold default to 1000
     %Set values of ones to 256 for easier division later on
     image = readLogicalImage('nutsbolts') * 256;
     while any(any(image == 256))
         % Find indices in the image with value 256
         [row, col] = find(image == 256);
         
-        % Pick a random element
-        % TODO: is this a morphological or set operation?
-        randomIdx = randi(size(row, 1));
-        randomRow = row(randomIdx);
-        randomCol = col(randomIdx);
+        % Pick a first 256 element
+        randomRow = row(1);
+        randomCol = col(1);
         
         % Set the mask
         mask = zeros(size(image));
         mask(randomRow, randomCol) = 256;
         
-        % TODO: implement IPrecon_by_dilation
-        % It is either 128 or 64 as result, so divide by 2
-        reconstructed = IPrecon_by_dilation(image, mask, [1,1,1;1,1,1;1,1,1]) / 2;
-        
+        % IPrecon_by_dilation returns a logical and we want definitely
+        % something that is either 128 or 64, so multiply by 64
+        reconstructed = IPrecon_by_dilation(mask, image, [256,256,256;256,256,256;256,256,256]) * 128;
+
         % If the number of pixels is smaller than threshold (so bolt)
-        % divide by 2 again
-        if size((any(reconstructed) >= 1), 1) < threshold
+        % divide by 2 again so result is 64 instead of 128
+        if size(find(reconstructed >= 1), 1) < threshold
             reconstructed = reconstructed / 2;
         end
         
         %Set the appropriate elements to their correct value
-        %TODO: can this be done without for loops? 
-        %example input:
-        %image = [256, 0, 0; 0, 0, 256];
-        %recon = [64, 0, 0; 0, 0, 0];
-        %desired output: [64, 0, 0; 0, 0, 256];
-        [M, N] = size(reconstructed);
-        for i=1:M
-            for j=1:N
-                if reconstructed(i, j) ~= 0
-                    image(i, j) = reconstructed(i, j);
-                end
-            end
-        end
+        %Find returns the index vector of nonzero elements
+        s = find(reconstructed);
+        image(s) = reconstructed(s);
     end
     outImage = image;
 end
