ebc39221464b8694ab9b9b5d6eccd202ec7a4ed7
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2015-01-07 16:04:03 +0100

    New images, fixed code (something weird with logicals)

--- week4/IPclassify.m
+++ week4/IPclassify.m
@@ -1,26 +1,22 @@
 function outImage = IPclassify(threshold)
     %Threshold default to 1000
-    %Set values of ones to 256 for easier division later on
-    image = readLogicalImage('nutsbolts') * 256;
-    while any(any(image == 256))
-        % Find indices in the image with value 256
-        [row, col] = find(image == 256);
-        
-        % Pick a first 256 element
-        randomRow = row(1);
-        randomCol = col(1);
+    %Don't use logical values
+    image = single(readLogicalImage('nutsbolts'));
+    while any(any(image == 1))
+        % Find indices in the image with value 1
+        [row, col] = find(image == 1);
         
         % Set the mask
         mask = zeros(size(image));
-        mask(randomRow, randomCol) = 256;
+        mask(row(1), col(1)) = 1;
         
         % IPrecon_by_dilation returns a logical and we want definitely
         % something that is either 128 or 64, so multiply by 64
-        reconstructed = IPrecon_by_dilation(mask, image, [256,256,256;256,256,256;256,256,256]) * 128;
-
+        reconstructed = IPrecon_by_dilation(mask, image, [1,1,1;1,1,1;1,1,1]) * 128;
+        
         % If the number of pixels is smaller than threshold (so bolt)
         % divide by 2 again so result is 64 instead of 128
-        if size(find(reconstructed >= 1), 1) < threshold
+        if size(find(reconstructed == 128), 1) < threshold
             reconstructed = reconstructed / 2;
         end
         
