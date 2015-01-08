216ebe4ca6842d20845a4882a180e3830b8edc58
hankruiger <hankruiger@gmail.com>
2015-01-08 15:33:56 +0100

    Wrote exercise 3

--- week4/ex3.m
+++ week4/ex3.m
@@ -1,41 +1,35 @@
 figure;
 
 angio = readLogicalImage('angio');
-subplot(2, 2, 1);
+subplot(2, 1, 1);
 imshow(angio);
 title('Original non-noisy angiogram');
 
 angio_noise = readLogicalImage('angio_noise');
-subplot(2, 2, 2);
+subplot(2, 1, 2);
 imshow(angio_noise);
 title('Noisy angiogram');
 
+figure;
+
 angio_noise_opened = IPopen(angio_noise, ones(3, 3));
-subplot(2, 2, 3);
+subplot(2, 1, 1);
 imshow(angio_noise_opened);
 title('Opened noisy angiogram');
 
 diff_opening = angio_noise_opened ~= angio;
-subplot(2, 2, 4);
+subplot(2, 1, 2);
 imshow(diff_opening);
 title(['Difference with non-noisy angiogram (', num2str(sum(sum(diff_opening))) ')']);
 
 figure;
 
-subplot(2, 2, 1);
-imshow(angio);
-title('Original non-noisy angiogram');
-
-subplot(2, 2, 2);
-imshow(angio_noise);
-title('Noisy angiogram');
-
 angio_opened_by_recon = IPrecon_by_dilation(IPerode(angio_noise, ones(3, 3)), angio_noise, ones(3, 3));
-subplot(2, 2, 3);
+subplot(2, 1, 1);
 imshow(angio_opened_by_recon);
 title('Noisy angiogram opened by reconstr.');
 
 diff_opening_by_recon = angio_opened_by_recon ~= angio;
-subplot(2, 2, 4);
+subplot(2, 1, 2);
 imshow(diff_opening_by_recon);
 title(['Difference with non-noisy angiogram (', num2str(sum(sum(diff_opening_by_recon))) ')']);
