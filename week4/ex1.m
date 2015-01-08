8dfa52a1c412f79447b703e91478f7e783d526ba
hankruiger <hankruiger@gmail.com>
2015-01-08 16:12:00 +0100

    Some changes

--- week4/ex1.m
+++ week4/ex1.m
@@ -1,26 +1,29 @@
 wirebond = readLogicalImage('wirebondmask');
 
 figure;
-subplot(3, 2, 1:2);
 imshow(wirebond);
 title('Original image');
 
-plusSE = logical([0, 1, 0; 1, 1, 1; 0, 1, 0]);
-
-subplot(3, 2, 3);
-imshow(IPdilate(wirebond, plusSE));
-title('Dilated with plus');
-
-subplot(3, 2, 4);
-imshow(IPerode(wirebond, plusSE));
-title('Eroded with plus');
-
+crossSE = logical([0, 1, 0; 1, 1, 1; 0, 1, 0]);
 blockSE = true(3, 3);
 
-subplot(3, 2, 5);
+figure;
+
+subplot(2, 1, 1);
+imshow(IPerode(wirebond, crossSE));
+title('Eroded with cross');
+
+subplot(2, 1, 2);
+imshow(IPerode(wirebond, blockSE));
+title('Eroded with block');
+
+figure;
+
+subplot(2, 1, 1);
+imshow(IPdilate(wirebond, crossSE));
+title('Dilated with cross');
+
+subplot(2, 1, 2);
 imshow(IPdilate(wirebond, blockSE));
 title('Dilated with block');
 
-subplot(3, 2, 6);
-imshow(IPerode(wirebond, blockSE));
-title('Eroded with block');
