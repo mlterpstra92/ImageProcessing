276ddd9013ccbbdd046e253a9fb318ce8df6fc2b
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2015-01-08 15:36:19 +0100

    Written part 1 and 2 of report

--- week4/IPdilate.m
+++ week4/IPdilate.m
@@ -10,14 +10,13 @@ function dilated = IPdilate(image, structuringElement )
         for j=2:N+1
             % The origin of the SE is now at (i-1, j-1) in `image' and at
             % (i, j) in `dilated'.
-            if image(i - 1 , j - 1);
+            if image(i - 1 , j - 1)
                 % The image pixel SE's origin is 1, so set all pixels where
                 % the SE is 1 to 1 in the dilated image as well.
                 dilated(i-1:i+1, j-1:j+1) = (dilated(i-1:i+1, j-1:j+1) | structuringElement);
             end
         end
     end
-    
     % Slice the zero-padding off the output image.
     dilated = dilated(2:M + 1, 2:N + 1);
 end
