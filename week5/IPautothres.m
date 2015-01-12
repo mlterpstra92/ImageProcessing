ee532dd44046956f65ea2bace5b51527cda165b4
hankruiger <hankruiger@gmail.com>
2015-01-12 21:24:20 +0100

    Changed initial guess to the mean of the image (as the book suggests)

--- week5/IPautothres.m
+++ week5/IPautothres.m
@@ -2,28 +2,27 @@ function [image, threshold] = IPautothres(image, deltaT)
     if nargin < 2
         deltaT = 0.1;
     end
-    % Initial threshold guess.
-    threshold = (max(image(:)) - min(image(:))) / 2;
+    % Use the mean as initial threshold estimate
+    threshold = mean(image(:));
     previousThreshold = Inf;
     
-    % Continue when there is enough change in the threshold
+    % Continue while there is still too much change in the threshold
     while abs(threshold - previousThreshold) > deltaT
-        %Segment the image based on the threshold
+        % Segment the image based on the current threshold
         G_1 = image(image > threshold);
         G_2 = image(image <= threshold);
         
-        size(G_1)
+        % Calculate the averages of the segmentations
+        m_1 = mean(G_1);
+        m_2 = mean(G_2);
         
-        %Calculate the averages of the segmentations
-        firstMean = mean(G_1);
-        secondMean = mean(G_2);
-        
-        %Continue iteration with new guess
+        % Continue iteration with the more accurate threshold
         previousThreshold = threshold;
-        threshold = (firstMean + secondMean) / 2;
+        threshold = (m_1 + m_2) / 2;
     end
     
-    %If the threshold is determined, actually segment the image
+    % When the threshold is determined, return the segmented image using
+    % this threshold.
     image = (image > threshold);
 end
 
