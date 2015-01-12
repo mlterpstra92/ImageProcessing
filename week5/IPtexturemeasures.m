fb38931b81616702c3386a67fffd6d753c3bc9d3
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2015-01-12 14:27:04 +0100

    fixed measures

--- week5/IPtexturemeasures.m
+++ week5/IPtexturemeasures.m
@@ -1,24 +1,23 @@
-function [ m, mu_n, r, skewedness, uniformity, entropy ] = IPtexturemeasures( image, n )
+function [ m, r, skewedness, uniformity, entropy ] = IPtexturemeasures( image)
     hist = zeros(1, 255);
     [M, N] = size(image);
     for i=1:M
         for j=1:N
-            hist(image(i, j)) = hist(image(i, j)) + 1;
+            if(image(i, j) > 0)
+                hist(image(i, j)) = hist(image(i, j)) + 1;
+            end
         end
     end
-    m = mean(hist);
-    hist_normalized_sq = hist / (255.0 * 255.0);
-    hist_normalized = hist / 255.0;
-    if nargin > 1
-        mu_n = mu(hist, n, m);
-    else 
-        mu_n = 0;
+    m = mean(image(:));
+    hist_normalized = hist / sum(hist(:));
+    variance = 0;
+    skewedness = 0;
+    for z=1:length(hist)
+        variance = variance + ((z - 1 -m) / length(hist_normalized - 1))^2 * hist_normalized(z);
+        skewedness = skewedness + ((z - 1 -m) / length(hist_normalized - 1))^3 * hist_normalized(z);
     end
-    
-    r = 1 - (1 / (1 + mu(hist_normalized_sq, 2, m)));
-    
-    skewedness = mu(hist_normalized, 3, m);
-    uniformity = sum(hist_normalized .* hist_normalized);
+    r = 1 - (1 / (1 + variance));
+    uniformity = sum(hist_normalized(:) .^ 2);
     entropy = -sum((hist_normalized(hist_normalized > 0)) .* log2(hist_normalized(hist_normalized > 0)));
 end
 
--- week5/ex2.m
+++ week5/ex2.m
@@ -1,12 +1,11 @@
 bubbles = readDoubleImage('bubbles');
 cktboard = readDoubleImage('cktboard');
 cereal = readDoubleImage('cereal');
-
 %extract lower right segment
 bubbles = bubbles(end - 100:end, end-100:end);
 cktboard = cktboard(end - 100:end, end-100:end);
 cereal = cereal(end - 100:end, end-100:end);
 
-[ m, mu_n, r, skewedness, uniformity, entropy ] = IPtexturemeasures(bubbles, 1)
-[ m, mu_n, r, skewedness, uniformity, entropy ] = IPtexturemeasures(cktboard, 1)
-[ m, mu_n, r, skewedness, uniformity, entropy ] = IPtexturemeasures(cereal, 1)
+[ m_b, r_b, skewedness_b, uniformity_b, entropy_b ] = IPtexturemeasures(bubbles)
+[ m_cb, r_cb, skewedness_cb, uniformity_cb, entropy_cb ] = IPtexturemeasures(cktboard)
+[ m_ce, r_ce, skewedness_ce, uniformity_ce, entropy_ce ] = IPtexturemeasures(cereal)
